9e0c433d832b019941f22a830848ce3e8dfa61ba
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-08 12:01:25 +0100

    fix double quotes on echo to html to php

--- php/login.php
+++ php/login.php
@@ -11,39 +11,39 @@ session_start();
 
 <body>
     <?php
-    echo"<div class="nav">
-        <div class="logo">
+    echo"<div class='nav'>
+        <div class='logo'>
             Logo
         </div>
-        <div class="nav-links">
-            <a href="Index.html">Homepage</a>
-            <a href="register.html">Register</a>
+        <div class='nav-links'>
+            <a href='Index.html'>Homepage</a>
+            <a href='register.html'>Register</a>
         </div>
     
     </div>
-    <div class="login">
-            <div class="form form-box">
+    <div class='login'>
+            <div class='form form-box'>
                 <h3>Login</h3>
-                <form action="" method="post">
-                    <div class="image"></div>
-                    <div class="field input">
-                        <label for="email">Email</label>
-                        <input type="text" name="email" id="email" required>
+                <form action='' method='post'>
+                    <div class='image'></div>
+                    <div class='field input'>
+                        <label for='email'>Email</label>
+                        <input type='text' name='email' id='email' required>
                     </div>
-                    <div class="field input">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" required>
+                    <div class='field input'>
+                        <label for='password'>Password</label>
+                        <input type='password' name='password' id='password' required>
                     </div>
-                    <div class="field">
-                        <input type="submit" name="submit" class="button"value="Login" required>
+                    <div class='field'>
+                        <input type='submit' name='submit' class='button'value='Login' required>
                     </div>
-                    <div class="links">
-                        Not a member yet? <a href="register.html">Join our community</a>
+                    <div class='links'>
+                        Not a member yet? <a href='register.html'>Join our community</a>
                     </div>
                 </form>
             </div>
         </div>
-    <!--<a href="subscribe.html">Subscribe</a>-->"
+    <!--<a href='subscribe.html'>Subscribe</a>-->"
     ?>
 </body>
 </html>
