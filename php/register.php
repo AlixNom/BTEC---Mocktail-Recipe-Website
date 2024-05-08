8ce470ff249c750174966cd4045a9bf7fa981516
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-08 16:40:20 +0100

    [Added] SQL Inject

--- php/register.php
+++ php/register.php
@@ -8,9 +8,9 @@
 <body>
     <?php
     include 'includes/connDB.php';
-    //$sql = "INSERT INTO mocktail_users (uname, name , email, socials) VALUES (?, ?, ?, ?)";
-    //$stmt = $conn->prepare($sql);
-    //$stmt->bind_param("ssss", $uname, $name, $email, $socials);
+    $sql = "INSERT INTO mocktail_users (uname, name , email, socials) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $uname, $name, $email, $socials);
     echo'
     <div class="nav">
         <div class="logo">
