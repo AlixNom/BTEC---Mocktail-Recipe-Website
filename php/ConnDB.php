a2e5e1f8aa7a14d3ebd812b98c686fb93e03b703
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-22 11:57:39 +0100

    test

---
+++ php/ConnDB.php
@@ -0,0 +1,14 @@
+<?php
+
+// Include database settings
+include 'config.php';
+
+// Create a connection to the database
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connected to database successfully!";
+}
