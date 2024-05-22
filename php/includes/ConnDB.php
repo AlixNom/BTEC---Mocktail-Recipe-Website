316089a2005213b2cc245b2dd37e8a00ca3b00bb
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-22 16:23:21 +0100

    ..

--- php/includes/ConnDB.php
+++ php/includes/ConnDB.php
@@ -10,5 +10,5 @@ $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }else{
-    echo "Connected to database successfully!";
+    echo "Connected to database successfully!..";
 }
