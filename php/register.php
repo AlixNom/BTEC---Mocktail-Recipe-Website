939fc5665fc6cbdaced7a1d9c4a40b67fe18579b
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-22 11:42:40 +0100

    [Fixed] file location db

--- php/register.php
+++ php/register.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <?php
-    require('ConnDB');
+    require('ConnDB.php');
         if(isset($_REQUEST['uname'])) {
             $uname = stripslashes($_REQUEST['uname']);
             $uname = mysqli_real_escape_string($conn, $uname);
