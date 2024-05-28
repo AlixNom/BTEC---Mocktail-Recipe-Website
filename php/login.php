dba56ff1841f94e40ec705bc8f92c52aae0bc63b
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-28 20:27:20 +0100

    ADDED Flash Message Login.php

--- php/login.php
+++ php/login.php
@@ -28,6 +28,9 @@ if(isset($_SESSION['status'])) {
 ?>
     <div class="alert">
         <strong>Hey!</strong> <?php echo $_SESSION['status'];?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+  </button>
     </div>
 <?php unset($_SESSION['status']); } ?>
 
