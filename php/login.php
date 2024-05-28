0a0b2434316a5d6dfe8c1cb21cae858152083aa1
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-28 18:03:05 +0100

    .

--- php/login.php
+++ php/login.php
@@ -27,10 +27,10 @@ session_start();
     if(isset($_SESSION['status'])) {
     ?>
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>Hey!</strong> <?php echo $_SESSION['status'];?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-    </button>
+        <strong>Hey!</strong> <?php echo $_SESSION['status'];?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
     </div>
     <div class='login'>
             <div class='form form-box'>
