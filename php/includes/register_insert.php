7a65d02b97032c2e1094d5e6e41e514e301e47ca
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-29 13:36:47 +0100

    Connect to Child table mocktail_passwords

--- php/includes/register_insert.php
+++ php/includes/register_insert.php
@@ -15,6 +15,9 @@ $email = stripslashes($_POST['email']);
 $email = mysqli_real_escape_string($conn, $email);
 $dob = date("Y-m-d H:i:s");
 
+$password = $_POST('password');
+$pword_hash = password_hash($password, PASSWORD_DEFAULT);
+
 $userVal = "SELECT * from mocktail_users where uname = '$uname'";
 $result = mysqli_query($conn, $userVal);
 $count = mysqli_num_rows($result);
@@ -23,10 +26,16 @@ If($count>0){
     $_SESSION['status'] = "There is already a user with the same username!";
     header("Location: ../register.php");
 } else {
+    $userPass = "SELECT id from mocktail_users where uname = '$uname'";
+    $result = $conn->prepare($userPass);
+    $resultPass = $result->fetch_assoc();
     $sql = "INSERT INTO mocktail_users (uname, name , surname, email, dob) VALUES (?, ?, ?, ? ,?)";
+    $sqlPass = "INSERT INTO mocktail_passwords (id, pword_hash ) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
+    $stmtPass = $conn->prepare($sqlPass);
 
     $stmt->bind_param("sssss", $uname, $name, $surname, $email, $dob);
+    $stmtPass->bind_param("ss", $row["id"], $pword_hash);
     if ($stmt->execute()) {
         $_SESSION['status'] = "Successfully registered as a user!";
         header("Location: ../login.php");
