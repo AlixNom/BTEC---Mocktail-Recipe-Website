6f905cdd621293989402706243e2d559c079d0e9
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-29 13:52:31 +0100

    .

--- php/includes/register_insert.php
+++ php/includes/register_insert.php
@@ -15,10 +15,6 @@ $email = stripslashes($_POST['email']);
 $email = mysqli_real_escape_string($conn, $email);
 $dob = date("Y-m-d H:i:s");
 
-//$pword_hash = stripslashes($_POST('password'));
-//$pword_hash = mysqli_real_escape_string($conn, $pword_hash);
-//$pword_hash = password_hash($_POST('password'), PASSWORD_DEFAULT);
-
 $userVal = "SELECT * from mocktail_users where uname = '$uname'";
 $result = mysqli_query($conn, $userVal);
 $count = mysqli_num_rows($result);
@@ -27,21 +23,24 @@ If($count>0){
     $_SESSION['status'] = "There is already a user with the same username!";
     header("Location: ../register.php");
 } else {
-    //$pword_hash = stripslashes($_POST('password'));
-    //$pword_hash = mysqli_real_escape_string($conn, $pword_hash);
-    //$pword_hash = password_hash($_POST('password'), PASSWORD_DEFAULT);
 
-    $userPass = "SELECT id from mocktail_users where uname = '$uname'";
-    $result = $conn->prepare($userPass);
-    $resultPass = $result->fetch_assoc();
     $sql = "INSERT INTO mocktail_users (uname, name , surname, email, dob) VALUES (?, ?, ?, ? ,?)";
-    $sqlPass = "INSERT INTO mocktail_passwords (id, pword_hash ) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmtPass = $conn->prepare($sqlPass);
 
     $stmt->bind_param("sssss", $uname, $name, $surname, $email, $dob);
-    $stmtPass->bind_param("ss", $row["id"], $pword_hash);
+    
     if ($stmt->execute()) {
+        $pword_hash = stripslashes($_POST('password'));
+        $pword_hash = mysqli_real_escape_string($conn, $pword_hash);
+        $pword_hash = password_hash($_POST('password'), PASSWORD_DEFAULT);
+
+        $userPass = "SELECT id from mocktail_users where uname = '$uname'";
+        $result = $conn->prepare($userPass);
+        $resultPass = $result->fetch_assoc();
+        $sqlPass = "INSERT INTO mocktail_passwords (id, pword_hash ) VALUES (?, ?)";
+        $stmtPass = $conn->prepare($sqlPass);
+        $stmtPass->bind_param("ss", $row["id"], $pword_hash);
+        
         $_SESSION['status'] = "Successfully registered as a user!";
         header("Location: ../login.php");
     } else {
