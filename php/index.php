c80b7ce0b406476a5a3649dd4a1aa99cbdc095a5
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 20:18:14 +0100

    .

--- php/index.php
+++ php/index.php
@@ -47,13 +47,13 @@ session_start();
             <div class="col">
                 <img src="img/mocktail-explore2.jpg" alt="">
                 <h4>Cocktails</h4>
-                <p>Explore the varieties of cocktail recipes made by multiple of users, You'll never get tired!</p>
+                <p>Dive into the diverse world of cocktail recipes crafted by countless enthusiasts. With such a wide array of options, you’ll always find something new and exciting to try, ensuring you'll never get tired of experimenting and discovering new flavors!</p>
                 <a href="#" class="ctn">View More</a>
             </div>
             <div class="col">
                 <img src="img/mocktail-submit.jpg" alt="">
                 <h4>Submit Your Own</h4>
-                <p>Want to share your own homemade cocktail recipes for others to enjoy?</p>
+                <p>Would you like to share your own homemade cocktail recipes for others to enjoy and appreciate? Join our vibrant community of mixology enthusiasts and showcase your unique creations. Your inventive concoctions could inspire fellow cocktail lovers and become the next big hit!</p>
                 <a href="#" class="ctn">Make Recipe</a>
             </div>
         </div>
