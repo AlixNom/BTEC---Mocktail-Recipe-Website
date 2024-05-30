d73c52ee406805d1df8c961a2e2be9b70bcdffb2
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 14:44:25 +0100

    .

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-home.css">
     <title>Homepage</title>
 </head>
 
