4fd9c77e3fd176bfa5ef9ae80a74e98e9f555c30
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 21:35:47 +0100

    .

--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -22,16 +22,14 @@
             <div class='form form-box'>
                 <h3>Make a Cocktail</h3>
                 <form action='includes/login_validation.php' method='post'>
-                    <div class="field input">
-                    <input list="options" name='combobox' id="combobox">
-                    <datalist id="options">
+                    <div class="field input" id="combobox-container">
+                    <input name='combobox'>
                         <select onchange="addComboBox(this)">
                             <option value="">Select an option</option>
                             <option value="option1">Option 1</option>
                             <option value="option2">Option 2</option>
                             <option value="option3">Option 3</option>
                         </select>
-                    </datalist>
                     </div>
                     <div class='field'>
                         <input type='submit' name='submit' class='button'value='Submit Recipe' required>
