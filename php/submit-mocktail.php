bb5cd8fec582481bbf299bd2e0104f77f860c066
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 21:47:47 +0100

    .

--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -22,13 +22,26 @@
             <div class='form form-box'>
                 <h3>Make a Cocktail</h3>
                 <form action='includes/login_validation.php' method='post'>
-                    <div class="field input" name ="combobox" id="combobox-container">
-                        <label for="sprite">Sprite</label>
+                    <div class="field input" name ="combobox-sprite" id="combobox-container-sprite">
+                        <label for="sprite">Base Sprite</label>
                         <select onchange="addComboBox(this)">
-                            <option value="">Select an option</option>
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
+                            <option value="">Select an Base Sprite</option>
+                            <option value="option1">Brandy</option>
+                            <option value="option2">Gin</option>
+                            <option value="option3">Rum</option>
+                            <option value="option3">Rhye Whiskey</option>
+                            <option value="option3">Tequila</option>
+                            <option value="option3">Vodka</option>
+                            <option value="option3">Whiskey</option>
+                        </select>
+                    </div>
+                    <div class="field input" name ="combobox-wine" id="combobox-container-wine">
+                        <label for="sprite">Wine</label>
+                        <select onchange="addComboBox(this)">
+                            <option value="">Select an Wine</option>
+                            <option value="option1">Dry White Wine</option>
+                            <option value="option2">Red Wine</option>
+                            <option value="option3">Sherry</option>
                         </select>
                     </div>
                     <div class='field'>
