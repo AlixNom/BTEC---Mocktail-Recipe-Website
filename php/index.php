c1ff3b169718c302eae4813c3684e28b771a9420
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 20:03:55 +0100

    .

--- php/index.php
+++ php/index.php
@@ -58,7 +58,7 @@ session_start();
             </div>
         </div>
     </section>
-    <section class="tip&tricks">
+    <section class="tiptricks">
         <h1>SECRETS TO MAKING THE BEST COCKTAIL!</h1>
         <p>Crafting the perfect cocktail is an art form that blends creativity with precision. Whether you’re a novice mixologist or a seasoned bartender, there are numerous tips and tricks to enhance your cocktail-making skills. Here's a comprehensive guide to elevating your cocktail game:</p>
         <a href="#" class="ctn"> Learn More </a>
