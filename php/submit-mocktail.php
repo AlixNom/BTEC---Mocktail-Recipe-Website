8fc594dc1f56be5b9ae011fed25429abe62fb8c6
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 21:16:12 +0100

    .

--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Dynamic Comboboxes</title>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Submit Mocktail</title>
 </head>
 <body>
-    <div id="combobox-container">
+    <div class='nav'>
+            <div class='logo'>
+                Logo
+            </div>
+            <div class='nav-links'>
+                <a href='Index.php'>Homepage</a>
+                <a href='register.php'>Register</a>
+            </div>
+        
+    </div>
+    <div class="combo-multi" id="combobox-container">
         <select onchange="addComboBox(this)">
             <option value="">Select an option</option>
             <option value="option1">Option 1</option>
