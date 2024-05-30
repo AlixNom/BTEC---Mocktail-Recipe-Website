78908baea0efd10712de0502b35efe3b92b8d77d
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-30 13:45:46 +0100

    .

--- php/includes/login_validation.php
+++ php/includes/login_validation.php
@@ -10,18 +10,15 @@ $userVal = "SELECT id, uname from mocktail_users where uname = '$user'";
 
 
 $stmt= $conn->prepare($userVal);
-if($stmt->num_rows > 0){
+if ($stmt === false) {
+    die("Error preparing statement: " . $conn->error);
+}
+if ($stmt->execute() === false) {
+    die("Error executing statement: " . $stmt->error);
+}
+$result = $stmt->get_result();
+if( $stmt->num_rows > 0){
 
-        if ($stmt === false) {
-            $_SESSION['status-warning'] = "Incorrect Password/Username. Please Try Again!";
-            header("Location: ../login.php");
-        }
-        if ($stmt->execute() === false) {
-            $_SESSION['status-warning'] = "Incorrect Password/Username. Please Try Again!";
-            header("Location: ../login.php");
-        }
-
-        $result = $stmt->get_result();
         while ($rowUser = $result->fetch_assoc()) {
             $id = $rowUser['id'];
             $passVal = "SELECT pword_hash from mocktail_passwords where id = $id";
@@ -46,7 +43,7 @@ if($stmt->num_rows > 0){
                     $_SESSION['status-warning'] = "Incorrect Password/Username. Please Try Again!";
                     header("Location: ../login.php");
                 }}}
-            } else{ 
+            } else {
                 $_SESSION['status-warning'] = "Incorrect Password/Username. Please Try Again!";
                 header("Location: ../login.php");
             }
