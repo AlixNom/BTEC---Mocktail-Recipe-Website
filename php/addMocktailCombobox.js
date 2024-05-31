97b78f94c7c9d750402f53eb1fe3fe0fa2517b2e
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-31 09:54:24 +0100

    .

--- php/addMocktailCombobox.js
+++ php/addMocktailCombobox.js
@@ -2,12 +2,17 @@ function addComboBox(selectElement) {
     if (selectElement.value !== "") {
         // Create a new combobox
         const newSelect = document.createElement("select");
-        newSelect.innerHTML = `
+        var list =""
+        var i;
+        for (i = 0; i < x.length; i++) {
+            newSelect.innerHTML = "<option>" + txt + x.options[i].text + "</option>";
+          } 
+        /*newSelect.innerHTML = `
             <option value="">Select an option</option>
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
-        `;
+        `;*/
         newSelect.onchange = function() { addComboBox(this); };
 
         // Append the new combobox to the container
