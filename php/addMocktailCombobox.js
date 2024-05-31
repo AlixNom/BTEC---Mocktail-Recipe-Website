b750c4b6b465682a62d59bbc7fd04a970e0d7945
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-05-31 12:01:32 +0100

    .

--- php/addMocktailCombobox.js
+++ php/addMocktailCombobox.js
@@ -22,29 +22,15 @@ function addComboBox(selectElement) {
         const optionsHTML = selectElement.innerHTML;
         const currentID = selectElement.name;
 
-        const newComboForm = document.createElement("div");
-        newComboForm.className = "combo-form";
-
-        const textInput = document.createElement("input");
-        textInput.type = "text";
-        textInput.name = "amount";
-        textInput.placeholder = "e.g 0.5";
-
         const newSelect = document.createElement("select");
         newSelect.name = `${currentID}`;
         newSelect.innerHTML = optionsHTML;
         newSelect.onchange = function() { addComboBox(this); };
 
-        // Create a remove button
-        const removeButton = document.createElement("button");
-        removeButton.type = "button";
-        removeButton.innerText = "Remove";
-        removeButton.onclick = function() { removeComboBox(newComboForm); };
-        
-        // Append the wrapper to the container
+  
 
         const container = document.getElementById(`${currentID}`)
-        container.appendChild(newSelect,newComboForm);
+        container.appendChild(newSelect);
 
         selectElement.onchange = null;
     }
