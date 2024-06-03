3ef3ad6523ac8dfa382daafffa548097186ceb2d
AlixNom <alixzulueta@gmail.com>
2024-06-03 16:38:28 +0100

    .

--- php/addMocktail.js
+++ php/addMocktail.js
@@ -10,23 +10,23 @@ function removeIngredient(){
 function addIngredients(){
     const addName = document.createElement("input");
     addName.type = "text";
-    //addName.name = "ingredient";
+    addName.name = "ingredient";
     addName.placeholder = "Enter Ingredient";
 
     const addQuantity = document.createElement("input")
     addQuantity.type = "number";
-    //addQuantity.name = "quantity";
+    addQuantity.name = "quantity";
     addQuantity.placeholder = "Enter Quantity";
 
     const addSelect = document.createElement("select")
-    //addSelect.name = "measurement";
+    addSelect.name = "measurement";
     addSelect.innerHTML = `
     <option value="ounces">Ounces</option>
     <option value="milliliters">Milliliters</option>`;
 
-    // btnSave = document.createElement("a");
-    //btnSave.className="save";
-    //btnSave.innerHTML="Edit";
+    btnSave = document.createElement("a");
+    btnSave.className="save";
+    btnSave.innerHTML="Save";
 
     const btnDelete = document.createElement("a");
     btnDelete.className="delete";
