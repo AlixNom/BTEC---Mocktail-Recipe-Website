aef7175e4b9986d75ceadca59aa362572bddd43e
AlixNom <alixzulueta@gmail.com>
2024-06-03 16:36:09 +0100

    .

--- php/addMocktail.js
+++ php/addMocktail.js
@@ -41,7 +41,6 @@ function addIngredients(){
     flex.appendChild(addName);
     flex.appendChild(addQuantity);
     //flex.appendChild(addSelect);
-    flex.appendChild(btnSave);
     flex.appendChild(btnDelete);
 }
 
