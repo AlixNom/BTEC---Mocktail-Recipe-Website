7a051d9308e2cfb17acb0cb8dbfbb81c912aec01
AlixNom <alixzulueta@gmail.com>
2024-06-03 20:32:34 +0100

    .

--- php/addMocktail.js
+++ php/addMocktail.js
@@ -14,39 +14,40 @@ function removeIngredient(){
 
 function addIngredients(){
     if(intIngredient.value !== "" || intAmount.value !== "" ||intUnit.value !== ""){
-    const addName = document.createElement("input");
-    addName.type = "text";
-    addName.value = intIngredient.value;
-    addName.name = "ingredient";
-    addName.placeholder = "Enter Ingredient";
+        JSONingredients = {ingredient:intIngredient.value,amount:intAmount.value,units:intUnit.value};
+        const addName = document.createElement("input");
+        addName.type = "text";
+        addName.value = intIngredient.value;
+        addName.name = "ingredient";
+        addName.placeholder = "Enter Ingredient";
 
-    const addQuantity = document.createElement("input")
-    addQuantity.type = "number";
-    addQuantity.value = intAmount.value;
-    addQuantity.name = "quantity";
-    addQuantity.placeholder = "Enter Quantity";
+        const addQuantity = document.createElement("input")
+        addQuantity.type = "number";
+        addQuantity.value = intAmount.value;
+        addQuantity.name = "quantity";
+        addQuantity.placeholder = "Enter Quantity";
 
-    const addSelect = document.createElement("select")
-    addSelect.name = "measurement";
-    addSelect.value = intUnit.value;
-    addSelect.innerHTML = `
-    <option value="ounces">Ounces</option>
-    <option value="milliliters">Milliliters</option>`;
+        const addSelect = document.createElement("select")
+        addSelect.name = "measurement";
+        addSelect.value = intUnit.value;
+        addSelect.innerHTML = `
+        <option value="ounces">Ounces</option>
+        <option value="milliliters">Milliliters</option>`;
 
-    const btnDelete = document.createElement("a");
-    btnDelete.className="delete";
-    btnDelete.innerHTML="&times";
+        const btnDelete = document.createElement("a");
+        btnDelete.className="delete";
+        btnDelete.innerHTML="&times";
 
-    btnDelete.addEventListener("click", removeIngredient);
+        btnDelete.addEventListener("click", removeIngredient);
 
-    const flex = document.createElement("div");
-    flex.className="flex";
-    
-    ingredients.appendChild(flex);
-    flex.appendChild(addName);
-    flex.appendChild(addQuantity);
-    flex.appendChild(addSelect);
-    flex.appendChild(btnDelete);
+        const flex = document.createElement("div");
+        flex.className="flex";
+        
+        ingredients.appendChild(flex);
+        flex.appendChild(addName);
+        flex.appendChild(addQuantity);
+        flex.appendChild(addSelect);
+        flex.appendChild(btnDelete);
     }
 }
 
