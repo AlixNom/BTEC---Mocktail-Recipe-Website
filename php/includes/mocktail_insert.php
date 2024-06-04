1ef309a6c8a03fd6bad02cfcdd5ea4c9927be706
AlixNom <alixzulueta@gmail.com>
2024-06-04 07:24:27 +0100

    .

--- php/includes/mocktail_insert.php
+++ php/includes/mocktail_insert.php
@@ -3,9 +3,13 @@ session_start();
 
 include 'ConnDB.php';
 
+$json_data = stripslashes($_POST['q']);
+$json_data = mysqli_real_escape_string($conn, $json_data);
 
+$sql = "INSERT INTO mocktail_recipes (ingredients) VALUES (?)";
+$stmt = $conn->prepare($sql);
 
-
+$stmt->bind_param("s", $json_data);
 
 $stmt->close();
 $conn->close();
