808fb75bccf146012dd720d27060a253ec69d1a9
AlixNom <alixzulueta@gmail.com>
2024-06-04 09:48:19 +0100

    .

--- php/includes/mocktail_insert.php
+++ php/includes/mocktail_insert.php
@@ -4,7 +4,7 @@ session_start();
 include 'ConnDB.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $json_data = json_decode($_POST['data'], true);
-    $json_data = mysqli_real_escape_string($conn, $json_data);
+    //$json_data = mysqli_real_escape_string($conn, $json_data);
 
     $sql = "INSERT INTO test (ingredients) VALUES (?)";
     $stmt = $conn->prepare($sql);
