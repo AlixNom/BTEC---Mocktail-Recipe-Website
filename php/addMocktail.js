7cacd8f4a2dc19090c3367e0ae9d155b78dd2ced
AlixNom <alixzulueta@gmail.com>
2024-06-05 19:33:44 +0100

    .

--- php/addMocktail.js
+++ php/addMocktail.js
@@ -1,130 +1,70 @@
-const btnAdd = document.querySelector(".add");
-const ingredients = document.querySelector(".ingredientsList");
-const intIngredient = document.querySelector(".ingredient");
-const intAmount = document.querySelector(".amount");
-const intUnit = document.querySelector(".measurement");
-const btnSubmit = document.querySelector(".submit")
-//const flex = document.querySelector(".flex");
+let dataArray = [];
 
-let JSONIngredientList = {};
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.add').addEventListener('click', function() {
+        let valIngredient = document.querySelector('.ingredient').value;
+        let valAmount = document.querySelector('.amount').value;
+        let valMeasurement = document.querySelector('.measurement').value;
 
-function removeIngredient(){
-    this.parentElement.remove();
-}
+        if (valIngredient && valAmount && valMeasurement) {
+            dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
+            updateContainer();
+            document.querySelector('.ingredient').value = '';
+            document.querySelector('.amount').value = '';
+            document.querySelector('.measurement').value = '';
+        }
+    });
 
+    document.querySelector('.submit').addEventListener('click', function() {
+        fetch('includes/mocktail_insert.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: dataArray })
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert('Data saved successfully!');
+        })
+        .catch(error => console.error('Error:', error));
+    });
 
-function addIngredients(){
-    if(intIngredient.value !== "" || intAmount.value !== "" ||intUnit.value !== ""){
-        let JSONingredient = {name:intIngredient.value,amount:intAmount.value,unit:intUnit.value};
-        JSONIngredientList = {...JSONIngredientList,JSONingredient}
-        const addName = document.createElement("input");
-        addName.type = "text";
-        addName.value = intIngredient.value;
-        addName.name = "ingredient";
-        addName.placeholder = "Ingredient";
+    function updateContainer() {
+        let container = document.querySelector('.ingredientsList');
+        container.innerHTML = '';
+        dataArray.forEach(function(item, index) {
+            let div = document.createElement('div');
+            div.className = 'flex';
+            div.dataset.index = index;
+            div.innerHTML = `
+                <input type="text" value="${item.valIngredient}" class="item-input-1">
+                <input type="number" value="${item.valAmount}" class="item-input-2">
+                <select class="item-input-3">
+                    <option value="Ounces" ${item.valMeasurement === 'Ounces' ? 'selected' : ''}>Ounces</option>
+                    <option value="Milliliters" ${item.valMeasurement === 'Milliliters' ? 'selected' : ''}>Milliliters</option>
+                </select>
+                <span class="delete">&times;</span>
+            `;
+            container.appendChild(div);
+        });
 
-        const addQuantity = document.createElement("input")
-        addQuantity.type = "number";
-        addQuantity.value = intAmount.value;
-        addQuantity.name = "Amount";
-        addQuantity.placeholder = "Enter Quantity";
+        document.querySelectorAll('.delete').forEach(function(element) {
+            element.addEventListener('click', function() {
+                let index = this.parentElement.dataset.index;
+                dataArray.splice(index, 1);
+                updateContainer();
+            });
+        });
 
-        const addSelect = document.createElement("select")
-        addSelect.name = "measurement";
-        addSelect.value = intUnit.value;
-        addSelect.innerHTML = `
-        <option value="ounces">Ounces</option>
-        <option value="milliliters">Milliliters</option>`;
-
-        const btnDelete = document.createElement("a");
-        btnDelete.className="delete";
-        btnDelete.innerHTML="&times";
-
-        btnDelete.addEventListener("click", removeIngredient);
-
-        const flex = document.createElement("div");
-        flex.className="flex";
-        
-        ingredients.appendChild(flex);
-        flex.appendChild(addName);
-        flex.appendChild(addQuantity);
-        flex.appendChild(addSelect);
-        flex.appendChild(btnDelete);
+        document.querySelectorAll('.item-input-1, .item-input-2, .item-input-3').forEach(function(element) {
+            element.addEventListener('change', function() {
+                let index = this.parentElement.dataset.index;
+                let newValue1 = this.parentElement.querySelector('.item-input-1').value;
+                let newValue2 = this.parentElement.querySelector('.item-input-2').value;
+                let newValue3 = this.parentElement.querySelector('.item-input-3').value;
+                dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
+            });
+        });
     }
-};
-
-function submitIngredients(JSONingredientList) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function() {
-        JSONingredientList = this.responseText;
-    }
-    xmlhttp.open("POST", "mocktail_insert.php?q=" + str);
-    xmlhttp.send();
-}
-
-btnAdd.addEventListener("click", addIngredients);
-btnSubmit.addEventListener("click", submitIngredients);
-
-
-//--------------------------------------
-
-// let dataArray = [];
-
-// $(document).ready(function(){
-//     $('#add').on('click', function() {
-//         let valIngredient = $('#ingredient').val();
-//         let valAmount = $('#amount').val();
-//         let valMeasurement = $('#measurement').val();
-        
-//         if (valIngredient && valAmount && valMeasurement) {
-//             dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
-//             updateContainer();
-//             $('#ingredient').val('');
-//             $('#amount').val('');
-//             $('#measurement').val('');
-//         }
-//     });
-
-//     $('#submit').on('click', function() {
-//         $.ajax({
-//             url: 'process.php',
-//             type: 'POST',
-//             data: { data: JSON.stringify(dataArray) },
-//             success: function(response) {
-//                 alert('Data saved successfully!');
-//             }
-//         });
-//     });
-
-//     function updateContainer() {
-//         $('#flex').empty();
-//         dataArray.forEach(function(item, index) {
-//             $('#flex').append(
-//                 `<div class="flex" data-index="${index}">
-//                     <input type="text" value="${item.valIngredient}" class="item-input-1">
-//                     <input type="number" value="${item.valAmount}" class="item-input-2">
-//                     <select class="item-input-3">
-//                         <option value="option-1" ${item.valMeasurement === 'option-1' ? 'selected' : ''}>No Measurement</option>
-//                         <option value="option-2" ${item.valMeasurement === 'option-2' ? 'selected' : ''}>Ounces</option>
-//                         <option value="option-3" ${item.valMeasurement === 'option-3' ? 'selected' : ''}>Milliliters</option>
-//                     </select>
-//                     <button class="edit">Edit</button>
-//                     <button class="delete">Remove</button>
-//                 </div>`
-//             );
-//         });
-//         $('.edit').on('click', function() {
-//             let index = $(this).parent().data('index');
-//             let newValue1 = $(this).siblings('.item-input-1').val();
-//             let newValue2 = $(this).siblings('.item-input-2').val();
-//             let newValue3 = $(this).siblings('.item-input-3').val();
-//             dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
-//         });
-
-//         $('.delete').on('click', function() {
-//             let index = $(this).parent().data('index');
-//             dataArray.splice(index, 1);
-//             updateContainer();
-//         });
-//     }
-// });
+});
--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -50,134 +50,77 @@
     </div>
     
 
-    <script>
-                let dataArray = [];
+    <script scr ="addMocktail.js">
+//                 let dataArray = [];
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.add').addEventListener('click', function() {
-        let valIngredient = document.querySelector('.ingredient').value;
-        let valAmount = document.querySelector('.amount').value;
-        let valMeasurement = document.querySelector('.measurement').value;
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('.add').addEventListener('click', function() {
+//         let valIngredient = document.querySelector('.ingredient').value;
+//         let valAmount = document.querySelector('.amount').value;
+//         let valMeasurement = document.querySelector('.measurement').value;
 
-        if (valIngredient && valAmount && valMeasurement) {
-            dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
-            updateContainer();
-            document.querySelector('.ingredient').value = '';
-            document.querySelector('.amount').value = '';
-            document.querySelector('.measurement').value = '';
-        }
-    });
+//         if (valIngredient && valAmount && valMeasurement) {
+//             dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
+//             updateContainer();
+//             document.querySelector('.ingredient').value = '';
+//             document.querySelector('.amount').value = '';
+//             document.querySelector('.measurement').value = '';
+//         }
+//     });
 
-    document.querySelector('.submit').addEventListener('click', function() {
-        fetch('includes/mocktail_insert.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ data: dataArray })
-        })
-        .then(response => response.text())
-        .then(data => {
-            alert('Data saved successfully!');
-        })
-        .catch(error => console.error('Error:', error));
-    });
+//     document.querySelector('.submit').addEventListener('click', function() {
+//         fetch('includes/mocktail_insert.php', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ data: dataArray })
+//         })
+//         .then(response => response.text())
+//         .then(data => {
+//             alert('Data saved successfully!');
+//         })
+//         .catch(error => console.error('Error:', error));
+//     });
 
-    function updateContainer() {
-        let container = document.querySelector('.ingredientsList');
-        container.innerHTML = '';
-        dataArray.forEach(function(item, index) {
-            let div = document.createElement('div');
-            div.className = 'flex';
-            div.dataset.index = index;
-            div.innerHTML = `
-                <input type="text" value="${item.valIngredient}" class="item-input-1">
-                <input type="number" value="${item.valAmount}" class="item-input-2">
-                <select class="item-input-3">
-                    <option value="Ounces" ${item.valMeasurement === 'Ounces' ? 'selected' : ''}>Ounces</option>
-                    <option value="Milliliters" ${item.valMeasurement === 'Milliliters' ? 'selected' : ''}>Milliliters</option>
-                </select>
-                <span class="delete">&times;</span>
-            `;
-            container.appendChild(div);
-        });
+//     function updateContainer() {
+//         let container = document.querySelector('.ingredientsList');
+//         container.innerHTML = '';
+//         dataArray.forEach(function(item, index) {
+//             let div = document.createElement('div');
+//             div.className = 'flex';
+//             div.dataset.index = index;
+//             div.innerHTML = `
+//                 <input type="text" value="${item.valIngredient}" class="item-input-1">
+//                 <input type="number" value="${item.valAmount}" class="item-input-2">
+//                 <select class="item-input-3">
+//                     <option value="Ounces" ${item.valMeasurement === 'Ounces' ? 'selected' : ''}>Ounces</option>
+//                     <option value="Milliliters" ${item.valMeasurement === 'Milliliters' ? 'selected' : ''}>Milliliters</option>
+//                 </select>
+//                 <span class="delete">&times;</span>
+//             `;
+//             container.appendChild(div);
+//         });
 
-        document.querySelectorAll('.delete').forEach(function(element) {
-            element.addEventListener('click', function() {
-                let index = this.parentElement.dataset.index;
-                dataArray.splice(index, 1);
-                updateContainer();
-            });
-        });
+//         document.querySelectorAll('.delete').forEach(function(element) {
+//             element.addEventListener('click', function() {
+//                 let index = this.parentElement.dataset.index;
+//                 dataArray.splice(index, 1);
+//                 updateContainer();
+//             });
+//         });
 
-        document.querySelectorAll('.item-input-1, .item-input-2, .item-input-3').forEach(function(element) {
-            element.addEventListener('change', function() {
-                let index = this.parentElement.dataset.index;
-                let newValue1 = this.parentElement.querySelector('.item-input-1').value;
-                let newValue2 = this.parentElement.querySelector('.item-input-2').value;
-                let newValue3 = this.parentElement.querySelector('.item-input-3').value;
-                dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
-            });
-        });
-    }
-});
-    // let dataArray = [];
-
-    //  $(document).ready(function(){
-    //      $('.add').on('click', function() {
-    //          let valIngredient = $('.ingredient').val();
-    //          let valAmount = $('.amount').val();
-    //          let valMeasurement = $('.measurement').val();
-            
-    //          if (valIngredient && valAmount && valMeasurement) {
-    //              dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
-    //              updateContainer();
-    //              $('.ingredient').val('');
-    //              $('.amount').val('');
-    //              $('.measurement').val('');
-    //          }
-    //      });
-
-    //      $('.submit').on('click', function() {
-    //          $.ajax({
-    //              url: 'includes/mocktail_insert.php',
-    //              type: 'POST',
-    //              data: { data: JSON.stringify(dataArray) },
-    //              success: function(response) {
-    //                  alert('Data saved successfully!');
-    //              }
-    //          });
-    //      });
-
-    //      function updateContainer() {
-    //          $('.ingredientsList').empty();
-    //          dataArray.forEach(function(item, index) {
-    //              $('.ingredientsList').append(
-    //                  `<div class="flex" data-index="${index}">
-    //                      <input type="text" value="${item.valIngredient}" class="item-input-1">
-    //                      <input type="number" value="${item.valAmount}" class="item-input-2">
-    //                      <select class="item-input-3">
-    //                          <option value="option-2" ${item.valMeasurement === 'option-2' ? 'selected' : ''}>Ounces</option>
-    //                          <option value="option-3" ${item.valMeasurement === 'option-3' ? 'selected' : ''}>Milliliters</option>
-    //                      </select>
-    //                      <a class="delete">&times;</a>
-    //                  </div>`
-    //              );
-    //          });
-    //          $('.edit').on('click', function() {
-    //              let index = $(this).parent().data('index');
-    //              let newValue1 = $(this).siblings('.item-input-1').val();
-    //              let newValue2 = $(this).siblings('.item-input-2').val();
-    //              let newValue3 = $(this).siblings('.item-input-3').val();
-    //              dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
-    //          });
-
-    //          $('.delete').on('click', function() {
-    //              let index = $(this).parent().data('index');
-    //              dataArray.splice(index, 1);
-    //              updateContainer();
-    //          });
-    //      }
-    //  });</script>
+//         document.querySelectorAll('.item-input-1, .item-input-2, .item-input-3').forEach(function(element) {
+//             element.addEventListener('change', function() {
+//                 let index = this.parentElement.dataset.index;
+//                 let newValue1 = this.parentElement.querySelector('.item-input-1').value;
+//                 let newValue2 = this.parentElement.querySelector('.item-input-2').value;
+//                 let newValue3 = this.parentElement.querySelector('.item-input-3').value;
+//                 dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
+//             });
+//         });
+//     }
+// });
+ </script>
 </body>
 </html>
