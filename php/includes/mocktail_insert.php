ac9f8a617a904ea6e6d04af4d6ded051ad92b2f8
AlixNom <alixzulueta@gmail.com>
2024-06-05 20:25:43 +0100

    .

--- php/includes/mocktail_insert.php
+++ php/includes/mocktail_insert.php
@@ -8,15 +8,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_SESSION['user'])) {
         $userID = stripslashes($_SESSION['user']);
         $userID = mysqli_real_escape_string($conn, $userID);
-        $methods = stripslashes($_POST['method']);
+        $methods = stripslashes($_POST[$methodTxt]);
         $methods = mysqli_real_escape_string($conn, $methods);
 
     //$json_data = mysqli_real_escape_string($conn, $json_data);
     // if (isset($dataArray['data'])&& is_array($dataArray['data'])) {
-        $sql = "INSERT INTO mocktail_recipes (id, ingredients) VALUES (?, ?)";
+        $sql = "INSERT INTO mocktail_recipes (id, ingredients, method) VALUES (?, ?)";
         $stmt = $conn->prepare($sql);
 
-        $stmt->bind_param("ss", $userID, $json_data);
+        $stmt->bind_param("sss", $userID, $json_data, $methods);
 
         if ($stmt->execute()) {
             echo "Data saved successfully!";
