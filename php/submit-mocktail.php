b30abb6195ecd40f607cc46b8291c65b68d14a94
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-06-05 12:00:19 +0100

    .

--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -17,7 +17,7 @@
             </div>
         
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 
     <div class='login'>
             <div class='form form-box'>
