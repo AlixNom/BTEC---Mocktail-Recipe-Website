b9650d108e109866bb17210754cd2ad0acd2a6fb
AlixNom <119893810+AlixNom@users.noreply.github.com>
2024-06-05 13:26:41 +0100

    .

--- php/submit-mocktail.php
+++ php/submit-mocktail.php
@@ -17,7 +17,7 @@
             </div>
         
     </div>
-    <<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <div class='login'>
             <div class='form form-box'>
@@ -53,66 +53,66 @@
     </div>
     
 
-    <script>
-    let dataArray = [];
+    <script scr="addMocktail.js">
+    // let dataArray = [];
 
-    $(document).ready(function(){
-        $('.add').on('click', function() {
-            let valIngredient = $('.ingredient').val();
-            let valAmount = $('.amount').val();
-            let valMeasurement = $('.measurement').val();
+    // $(document).ready(function(){
+    //     $('.add').on('click', function() {
+    //         let valIngredient = $('.ingredient').val();
+    //         let valAmount = $('.amount').val();
+    //         let valMeasurement = $('.measurement').val();
             
-            if (valIngredient && valAmount && valMeasurement) {
-                dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
-                updateContainer();
-                $('.ingredient').val('');
-                $('.amount').val('');
-                $('.measurement').val('');
-            }
-        });
+    //         if (valIngredient && valAmount && valMeasurement) {
+    //             dataArray.push({ valIngredient: valIngredient, valAmount: valAmount, valMeasurement: valMeasurement });
+    //             updateContainer();
+    //             $('.ingredient').val('');
+    //             $('.amount').val('');
+    //             $('.measurement').val('');
+    //         }
+    //     });
 
-        $('.submit').on('click', function() {
-            $.ajax({
-                url: 'includes/mocktail_insert.php',
-                type: 'POST',
-                data: { data: JSON.stringify(dataArray) },
-                success: function(response) {
-                    alert('Data saved successfully!');
-                    echo `${data}`;
-                }
-            });
-        });
+    //     $('.submit').on('click', function() {
+    //         $.ajax({
+    //             url: 'includes/mocktail_insert.php',
+    //             type: 'POST',
+    //             data: { data: JSON.stringify(dataArray) },
+    //             success: function(response) {
+    //                 alert('Data saved successfully!');
+    //                 echo `${data}`;
+    //             }
+    //         });
+    //     });
 
-        function updateContainer() {
-            $('.ingredientsList').empty();
-            dataArray.forEach(function(item, index) {
-                $('.ingredientsList').append(
-                    `<div class="flex" data-index="${index}">
-                        <input type="text" value="${item.valIngredient}" class="item-input-1">
-                        <input type="number" value="${item.valAmount}" class="item-input-2">
-                        <select class="item-input-3">
-                            <option value="option-2" ${item.valMeasurement === 'option-2' ? 'selected' : ''}>Ounces</option>
-                            <option value="option-3" ${item.valMeasurement === 'option-3' ? 'selected' : ''}>Milliliters</option>
-                        </select>
-                        <a class="delete">&times;</a>
-                    </div>`
-                );
-            });
-            $('.edit').on('click', function() {
-                let index = $(this).parent().data('index');
-                let newValue1 = $(this).siblings('.item-input-1').val();
-                let newValue2 = $(this).siblings('.item-input-2').val();
-                let newValue3 = $(this).siblings('.item-input-3').val();
-                dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
-            });
+    //     function updateContainer() {
+    //         $('.ingredientsList').empty();
+    //         dataArray.forEach(function(item, index) {
+    //             $('.ingredientsList').append(
+    //                 `<div class="flex" data-index="${index}">
+    //                     <input type="text" value="${item.valIngredient}" class="item-input-1">
+    //                     <input type="number" value="${item.valAmount}" class="item-input-2">
+    //                     <select class="item-input-3">
+    //                         <option value="option-2" ${item.valMeasurement === 'option-2' ? 'selected' : ''}>Ounces</option>
+    //                         <option value="option-3" ${item.valMeasurement === 'option-3' ? 'selected' : ''}>Milliliters</option>
+    //                     </select>
+    //                     <a class="delete">&times;</a>
+    //                 </div>`
+    //             );
+    //         });
+    //         $('.edit').on('click', function() {
+    //             let index = $(this).parent().data('index');
+    //             let newValue1 = $(this).siblings('.item-input-1').val();
+    //             let newValue2 = $(this).siblings('.item-input-2').val();
+    //             let newValue3 = $(this).siblings('.item-input-3').val();
+    //             dataArray[index] = { valIngredient: newValue1, valAmount: newValue2, valMeasurement: newValue3 };
+    //         });
 
-            $('.delete').on('click', function() {
-                let index = $(this).parent().data('index');
-                dataArray.splice(index, 1);
-                updateContainer();
-            });
-        }
-    });
+    //         $('.delete').on('click', function() {
+    //             let index = $(this).parent().data('index');
+    //             dataArray.splice(index, 1);
+    //             updateContainer();
+    //         });
+    //     }
+    // });
     </script>
 </body>
 </html>
