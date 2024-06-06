2cdacb125d85823b12a02e00a7f5f4592bd3d55f
AlixNom <alixzulueta@gmail.com>
2024-06-06 22:36:21 +0100

    .

--- php/update-mocktail.php
+++ php/update-mocktail.php
@@ -49,26 +49,15 @@
                 <h3>Update Mocktail</h3>
                 </div>
                 <?php
-                $sql = msqli_query($)
+                $sql = msqli_query($conn, "SELECT * from mocktail_recipes");
+                while($row = mysqli_fetch_assoc($sql)){
                 ?>
                 <form action="includes/mocktail_update.php" method="post" enctype="multipart/form-data"> 
-                    <label>Ingredients</label>
-                    <div class="ingredientsList">	
-                    </div>
-                    <input type="hidden" name="ingredientArray" id="ingredientArray">
-                    <div class="flex-add">
-                            <input type ="text" class ="ingredient" id="ingredient" placeholder = "Ingredient"></input>
-                            <input type ="number" class ="amount" id="amount" placeholder = "Amount" min="1"></input>
-                            <select class = "measurement" id="measurement" >
-                                <option value="">Select Measurement</option>
-                                <option value="ounces">Ounces</option>
-                                <option value="milliliters">Milliliters</option>
-                            </select>
-                            <a href="#" class="add">&plus;</a>
-                    </div><br></br>
+                    <label><strong>Important!</strong>Ingredients cannot be updated!</label>
+                    <input type="hidden" name="ingredientArray" value="<?php $row['ingredients']?>" id="ingredientArray">
                     <div class='field input'>
                             <label>Title</label>
-                            <input type='text' class ='titleMocktail' name='titleMocktail' id='titleMocktail' maxlength="15" placeholder="Title" required>
+                            <input type='text' class ='titleMocktail' name='titleMocktail' value="<?php echo $row['title']?>" id='titleMocktail' maxlength="15" placeholder="Title" required>
                     </div>
                     <div class='field input'>
                             <label>Serving Amount</label>
@@ -95,64 +84,6 @@
     </div>
     
 
-    <script scr ="addMocktail.js">
-    let dataArray = [];
-
-    document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector('.add').addEventListener('click', function() {
-            let Ingredient = document.querySelector('.ingredient').value;
-            let Amount = document.querySelector('.amount').value;
-            let Unit = document.querySelector('.measurement').value;
-
-            if (Ingredient && Amount && Unit) {
-                dataArray.push({ Ingredient: Ingredient, Amount: Amount, Unit: Unit });
-                updateContainer();
-                document.querySelector('.ingredient').value = '';
-                document.querySelector('.amount').value = '';
-                document.querySelector('.measurement').value = '';
-            }
-        });
-
-        document.querySelector('.submit').addEventListener('click', function() {
-            let body = JSON.stringify({data: dataArray});
-            document.getElementById('ingredientArray').value = body;
-         });
-
-        function updateContainer() {
-            let container = document.querySelector('.ingredientsList');
-            container.innerHTML = '';
-            dataArray.forEach(function(item, index) {
-                let div = document.createElement('div');
-                div.className = 'flex';
-                div.dataset.index = index;
-                div.innerHTML = `
-                    <input type="text" value="${item.Ingredient}" class="item-input-1">
-                    <input type="number" value="${item.Amount}" class="item-input-2 min="1">
-                    <label class = "item-input-3" >${item.Unit}</label>
-                    <a class = "delete">&times;</a>
-                `;
-                container.appendChild(div);
-            });
-
-            document.querySelectorAll('.delete').forEach(function(element) {
-                element.addEventListener('click', function() {
-                    let index = this.parentElement.dataset.index;
-                    dataArray.splice(index, 1);
-                    updateContainer();
-                });
-            });
-
-            document.querySelectorAll('.item-input-1, .item-input-2, .item-input-3').forEach(function(element) {
-                element.addEventListener('change', function() {
-                    let index = this.parentElement.dataset.index;
-                    let newIngredient = this.parentElement.querySelector('.item-input-1').value;
-                    let newAmount = this.parentElement.querySelector('.item-input-2').value;
-                    let newUnit = this.parentElement.querySelector('.item-input-3').value;
-                    dataArray[index] = { Ingredient: newIngredient, Amount: newAmount, Unit: newUnit };
-                });
-            });
-        }
-    });
-    </script>
+  
 </body>
 </html>
