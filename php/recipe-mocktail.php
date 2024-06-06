b70909aba248500e01eb763fb3497c837c37da5b
AlixNom <alixzulueta@gmail.com>
2024-06-06 17:32:25 +0100

    .

--- php/recipe-mocktail.php
+++ php/recipe-mocktail.php
@@ -20,7 +20,7 @@ foreach ($ingredientArray as $ingredient) {
     $ingredients .= "-  $valIngredient       $valAmount$valUnit <br>";
 }
 $method = $row['method'];
-$method = nl2br($method)
+$method = nl2br($method);
 ?>
 <html lang="en">
 <head>
