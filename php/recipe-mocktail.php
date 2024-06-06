e4ef29597c5d8b7b75e45d9e522f0c4dd64b5f61
AlixNom <alixzulueta@gmail.com>
2024-06-06 14:16:58 +0100

    .

--- php/recipe-mocktail.php
+++ php/recipe-mocktail.php
@@ -1,66 +1,53 @@
 <?php
-    session_start();
-    include 'includes/ConnDB.php';
-
-    $sql = "SELECT * from mocktail_recipes";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
+session_start();
+//require_once('includes/register_insert.php')
+//include('includes/register_insert.php');
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style-browse.css">
-    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
 </head>
 
 <body>
 
-    <div class="nav">
-        <div class="logo">
+<div class='nav'>
+        <div class='logo'>
             Logo
         </div>
-        <div class="nav-links">
+        <div class='nav-links'>
             <a class="links" href="index.php">Home</a>
             <a class="links" href="login.php">Login</a>
             <a class="links" href="register.php">Not a Member?</a>
         </div>
-    </div>
-    <header>    
-        <!-- <div class="header-content">
-            <h2>Browse endless amount of mocktails!</h2>
-            <div class="line"></div>
-        </div> -->
-    </header>
-    <section class="cocktail-section">
-        <div class="title">
-            <h1>Recipes</h1>
-            <div class="line"></div>
-        </div>
-        <main>
-            <?php
-                while($row = $result->fetch_assoc()){
-            ?>
-            <div class = "card">
-                <div class = "image">
-                    <img scr="<?php echo $row['image'];?>" alt="">
-                </div>
-                <div class="caption">
-                    <p class = "name"><?php echo $row['title'];?></p>
-                    <p class = "serving">Serving: <?php echo $row['servings'];?></p>
-                    <p class = "description"><?php echo $row['description'];?></p>
-                </div>
-                <button class = "view"> View Recipe</button>
+    
+</div>
+
+    <div class='login'>
+            <div class='form form-box'>
+                <h3>Login</h3>
+                <form>
+                    <div class='field'>
+                        <a href ="browse-mocktail.php" class= "back">&times;</a>
+                    </div>
+                    <div class='image'></div>
+                    <div class='field input'>
+                        <label for='email'>User</label>
+                    </div>
+                    <div class='links'>
+                        Not a member yet? <a href='register.php'>Join our community</a>
+                    </div>
+                </form>
             </div>
-            <?php
-            }?>
-        </main>
-    </section>
+        </div>
+    <!--<a href='subscribe.php'>Subscribe</a>-->";
     <section class = "footer">
         <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
         <p>Copyright © 2024 Alexis Zulueta</p>
     </section>
 </body>
-
+</body>
 </html>
+<script type="text/javascript" src="onload.js"></script>
