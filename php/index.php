aff5499880bfdc7bdcdb543c53e4edf4b44d0ec2
AlixNom <alixzulueta@gmail.com>
2024-06-06 15:20:25 +0100

    .

--- php/index.php
+++ php/index.php
@@ -1,63 +1,102 @@
 <?php
 session_start();
-
-include('includes/ConnDB.php');
-$recipe_id = $_SESSION['recipe_uid'];
-$sql = "SELECT * FROM mocktail_recipes WHERE id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $recipe_id);
-$stmt->execute();
-$result = $stmt->get_result();
-$row = $result->fetch_assoc();
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Recipe Detail</title>
+    <link rel="stylesheet" href="style-home.css">
+    <title>Homepage</title>
 </head>
 
 <body>
 
-<div class='nav'>
-    <div class='logo'>Logo</div>
-    <div class='nav-links'>
-        <a class="links" href="index.php">Home</a>
-        <a class="links" href="login.php">Login</a>
-        <a class="links" href="register.php">Not a Member?</a>
-    </div>
-</div>
-
-<div class='login'>
-    <div class='form form-box'>
-        <div class='field'>
-            <a href="browse-mocktail.php" class="back">&times;</a>
+    <div class="nav">
+        <div class="logo">
+            Logo
         </div>
-        <h3><?php echo $row['title']; ?></h3>
-        <form>
-            <div class='image'><img src="<?php echo $row['image']; ?>" alt="Recipe Image"></div>
-            <div class='field input'>
-                <label class="creator">Made By ...</label>
-                <label class="desc"><?php echo $row['description']; ?></label>
-                <label class="servings">Serves <?php echo $row['servings']; ?> people</label>
-                <label>Ingredients</label>
-                <label class="ingredients"><?php echo $row['ingredients']; ?></label>
-                <label>Methods</label>
-                <label class="method"><?php echo $row['method']; ?></label>
-            </div>
-            <div class='links'>
-                Not a member yet? <a href='register.php'>Join our community</a>
-            </div>
-        </form>
+        <div class="nav-links">
+            <a class="links" href="index.php">Home</a>
+            <a class="links" href="login.php">Login</a>
+            <a class="links" href="register.php">Not a Member?</a>
+        </div>
     </div>
-</div>
-
-<section class="footer">
-    <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
-    <p>Copyright © 2024 Alexis Zulueta</p>
-</section>
-
+    <?php
+    if(isset($_SESSION['status'])) {
+    ?>
+        <div class="alert-success">
+            <strong>Hey!</strong> <?php echo $_SESSION['status'];?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php unset($_SESSION['status']); } ?>
+    <?php
+    if(isset($_SESSION['status-warning'])) {
+    ?>
+    <div class="alert-error">
+        <strong>Invalid!</strong> <?php echo $_SESSION['status-warning'];?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <?php unset($_SESSION['status-warning']); } ?>
+    <header>    
+        <div class="header-content">
+            <h2>Enjoy a cocktail with love ones!</h2>
+            <div class="line"></div>
+            <h1>LIVE LAUGH LOVE!</h1>
+            <a href="#" class="ctn">Newsletter</a>
+        </div>
+    </header>
+    <section class="cocktail-section">
+        <div class="title">
+            <h1>Explore</h1>
+            <div class="line"></div>
+        </div>
+        <div class ="row">
+            <div class="col">
+                <img src="img/mocktail-explore2.jpg" alt="">
+                <h4>Cocktails</h4>
+                <p>Dive into the diverse world of cocktail recipes crafted by countless enthusiasts. With such a wide array of options, you’ll always find something new and exciting to try, ensuring you'll never get tired of experimenting and discovering new flavors!</p>
+                <a href="browse-mocktail.php" class="ctn">Browse</a>
+            </div>
+            <div class="col">
+                <img src="img/mocktail-submit.jpg" alt="">
+                <h4>Submit Your Own</h4>
+                <p>Would you like to share your own homemade cocktail recipes for others to enjoy and appreciate? Join our vibrant community of mixology enthusiasts and showcase your unique creations. Your inventive concoctions could inspire fellow cocktail lovers and become the next big hit!</p>
+                <a href="submit-mocktail.php" class="ctn">Make Recipe</a>
+            </div>
+            
+        </div>
+        <div class ="row">
+            <div class="col">
+                <img src="img/mocktail-newspaper.jpg" alt="">
+                <h4>Newsletter</h4>
+                <p>about the latest trends and updates in the cocktail world? Keep up with the current cocktail updates and discover new recipes, techniques, and innovations in mixology.</p>
+                <a href="#" class="ctn">View More</a>
+            </div>
+            <div class="col">
+                <img src="img/mocktail-poll.jpg" alt="">
+                <h4>Polls/Quizzes</h4>
+                <p>Challenge yourself and see how much you really know about the art of mixology, or place your opinions out!</p>
+                <a href="#" class="ctn">View More</a>
+            </div>
+            
+        </div>
+    </section>
+    <section class="tiptricks">
+        <div class="tiptricks-content">
+        <h1>SECRETS TO MAKING THE BEST COCKTAIL!</h1>
+        <div class="line"></div>
+        <p>Crafting the perfect cocktail is an art form that blends creativity with precision, there are numerous tips and tricks to enhance your cocktail-making skills. Here's a comprehensive guide to elevating your cocktail game:</p>
+        <a href="#" class="ctn"> Learn More </a>
+        </div>
+    </section>
+    <section class = "footer">
+        <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
+        <p>Copyright © 2024 Alexis Zulueta</p>
+    </section>
 </body>
+
 </html>
-<script type="text/javascript" src="onload.js"></script>
--- php/recipe-mocktail.php
+++ php/recipe-mocktail.php
@@ -1,64 +1,63 @@
 <?php
-    session_start();
+session_start();
 
-    include('includes/ConnDB.php');
-    $sql = "SELECT * from mocktail_recipes where id = $_SESSION['recipe_uid']"
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while ($row = $result->fetch_assoc()) {
+include('includes/ConnDB.php');
+$recipe_id = stripslashes($_SESSION['recipe_uid']);
+$sql = "SELECT * FROM mocktail_recipes WHERE id = $recipe_id";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $recipe_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Login</title>
+    <title>Recipe Detail</title>
 </head>
 
 <body>
 
 <div class='nav'>
-        <div class='logo'>
-            Logo
-        </div>
-        <div class='nav-links'>
-            <a class="links" href="index.php">Home</a>
-            <a class="links" href="login.php">Login</a>
-            <a class="links" href="register.php">Not a Member?</a>
-        </div>
-    
+    <div class='logo'>Logo</div>
+    <div class='nav-links'>
+        <a class="links" href="index.php">Home</a>
+        <a class="links" href="login.php">Login</a>
+        <a class="links" href="register.php">Not a Member?</a>
+    </div>
 </div>
 
-    <div class='login'>
-            <div class='form form-box'>
-                <div class='field'>
-                        <a href ="browse-mocktail.php" class= "back">&times;</a>
-                </div>
-                <h3><?php $row['title']?></h3>
-                <form>
-                    <div class='image'>Image</div>
-                    <div class='field input'>
-                        <label class="creator">Made By ...</label>
-                        <label class="desc"><?php $row['description']?></label>
-                        <label class="servings">Serves <?php $row['servings']?> people</label>
-                        <label>Ingredients</label>
-                        <label class="ingredients"><?php $row['ingredients']?></label>
-                        <label >Methods</label>
-                        <label class="method"><?php $row['method']?></label>
-                    </div>
-                    <div class='links'>
-                        Not a member yet? <a href='register.php'>Join our community</a>
-                    </div>
-                </form>
-            </div>
+<div class='login'>
+    <div class='form form-box'>
+        <div class='field'>
+            <a href="browse-mocktail.php" class="back">&times;</a>
         </div>
-    <!--<a href='subscribe.php'>Subscribe</a>-->";
-    <section class = "footer">
-        <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
-        <p>Copyright © 2024 Alexis Zulueta</p>
-    </section>
-<?php}?>
+        <h3><?php echo $row['title']; ?></h3>
+        <form>
+            <div class='image'><img src="<?php echo $row['image']; ?>" alt="Recipe Image"></div>
+            <div class='field input'>
+                <label class="creator">Made By ...</label>
+                <label class="desc"><?php echo $row['description']; ?></label>
+                <label class="servings">Serves <?php echo $row['servings']; ?> people</label>
+                <label>Ingredients</label>
+                <label class="ingredients"><?php echo $row['ingredients']; ?></label>
+                <label>Methods</label>
+                <label class="method"><?php echo $row['method']; ?></label>
+            </div>
+            <div class='links'>
+                Not a member yet? <a href='register.php'>Join our community</a>
+            </div>
+        </form>
+    </div>
+</div>
+
+<section class="footer">
+    <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
+    <p>Copyright © 2024 Alexis Zulueta</p>
+</section>
+
 </body>
 </html>
-<script type="text/javascript" src="onload.js"></script>
+
