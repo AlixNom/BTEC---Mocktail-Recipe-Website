15f5e29efbef18073846d49b142cc9832661a92c
AlixNom <alixzulueta@gmail.com>
2024-06-06 10:44:29 +0100

    .

--- php/browse-mocktail.php
+++ php/browse-mocktail.php
@@ -28,7 +28,7 @@ session_start();
     </header>
     <section class="cocktail-section">
         <div class="title">
-            <h1>Explore</h1>
+            <h1>Browse Recipes</h1>
             <div class="line"></div>
         </div>
         <div class ="row">
