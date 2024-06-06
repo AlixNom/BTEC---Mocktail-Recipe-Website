dc242e107dc134b83ca8cf953c5fe61fa57200a6
AlixNom <alixzulueta@gmail.com>
2024-06-06 08:23:57 +0100

    .

--- php/index.php
+++ php/index.php
@@ -21,11 +21,10 @@ session_start();
         </div>
     </div>
     <?php
-    session_start();
     if(isset($_SESSION['status'])) {
     ?>
         <div class="alert-error">
-            <strong>Error!</strong> <?php echo $_SESSION['status'];?>
+            <strong>Hey!</strong> <?php echo $_SESSION['status'];?>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
