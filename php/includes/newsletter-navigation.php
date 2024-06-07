bb5330814ad8b60c77b3efcc4e40f71f736615c7
AlixNom <alixzulueta@gmail.com>
2024-06-07 11:24:09 +0100

    .

--- php/includes/newsletter-navigation.php
+++ php/includes/newsletter-navigation.php
@@ -3,7 +3,7 @@ session_start();
 include 'ConnDB.php';
 $url = "https://newsapi.org/v2/everything?q=mocktail&apiKey=f72e3318d86b4a52b2eea095123bc312";
 
-$data = json_encode($url);
+$data = header($url);
 
 $seasontmp = $_GET['season'];
 $_SESSION['season'] = $seasontmp;
