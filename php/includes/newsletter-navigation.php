ce0607fa4e531659c6205bedac8d3df6f3bc5333
AlixNom <alixzulueta@gmail.com>
2024-06-07 12:20:03 +0100

    .

--- php/includes/newsletter-navigation.php
+++ php/includes/newsletter-navigation.php
@@ -1,26 +1,22 @@
 <?php
 session_start();
 include 'ConnDB.php';
-$apiUrl = "https://newsapi.org/v2/everything?q=mocktail&apiKey=f72e3318d86b4a52b2eea095123bc312";
+$curl = curl_init();
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://newsapi.org/v2/everything?q=mocktail&apiKey=f72e3318d86b4a52b2eea095123bc312',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $apiUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
 
-
-$response = curl_exec($ch);
-
-
-if ($response === false) {
-    die('Error occurred: ' . curl_error($ch));
-}
-
-
-
-$data = json_decode($response, true);
-print_r($data);
-curl_close($ch);
+curl_close($curl);
 // if (isset($data['articles'])) {
 
 //     $articles = array_slice($data['articles'], 0, 3);
