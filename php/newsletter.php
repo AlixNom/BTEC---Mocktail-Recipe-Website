56ad8d14a6f811b945d410b7d0cffec6e8382ac4
AlixNom <alixzulueta@gmail.com>
2024-06-07 18:41:22 +0100

    .

--- php/newsletter.php
+++ php/newsletter.php
@@ -55,7 +55,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style-browse.css">
-    <title>Homepage</title>
+    <title>Newsletter</title>
 </head>
 
 <body>
