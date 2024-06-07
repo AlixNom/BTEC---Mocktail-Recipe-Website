ecf679b8d00de93aca3b434a1d9ce114981a812a
AlixNom <alixzulueta@gmail.com>
2024-06-07 12:57:04 +0100

    .

--- php/includes/newsletter-navigation.php
+++ php/includes/newsletter-navigation.php
@@ -27,18 +27,18 @@ $_SESSION['season'] = $seasontmp;
 
 $data = json_encode($response, true);
 $data = stripslashes($data);
-//echo $data;
-if (isset($data['articles']) && count($data['articles']) > 0) {
-    // Randomly select one article
-    $randomIndex = array_rand($data['articles']);
-    $article = $data['articles'][$randomIndex];
+echo $data;
+// if (isset($data['articles']) && count($data['articles']) > 0) {
+//     // Randomly select one article
+//     $randomIndex = array_rand($data['articles']);
+//     $article = $data['articles'][$randomIndex];
 
-    // Display the selected article
-    echo "Title: " . $article['title'] . "\n";
-    echo "Description: " . $article['description'] . "\n";
-    echo "URL: " . $article['url'] . "\n";
-} else {
-    echo "No articles found.\n";
-}
+//     // Display the selected article
+//     echo "Title: " . $article['title'] . "\n";
+//     echo "Description: " . $article['description'] . "\n";
+//     echo "URL: " . $article['url'] . "\n";
+// } else {
+//     echo "No articles found.\n";
+// }
 // header("Location: ../newsletter.php");
 ?>
