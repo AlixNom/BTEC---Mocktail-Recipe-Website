651705d18bab2bc2e0e007367fefa7a7ff235c2c
AlixNom <alixzulueta@gmail.com>
2024-06-07 13:44:01 +0100

    .

--- php/newsletter.php
+++ php/newsletter.php
@@ -4,7 +4,7 @@
 
     $season = stripslashes($_SESSION['season']);
     $season = mysqli_real_escape_string($conn, $season);
-    $sql = "SELECT * from mocktail_recipes where season = $season";
+    $sql = "SELECT * from mocktail_recipes where seasons = $season";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->get_result();
