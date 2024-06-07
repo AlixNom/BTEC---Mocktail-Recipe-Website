a70a0882ac7897901c5a31ce0054d20399794764
AlixNom <alixzulueta@gmail.com>
2024-06-07 12:51:50 +0100

    .

--- php/includes/newsletter-navigation.php
+++ php/includes/newsletter-navigation.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
 include 'ConnDB.php';
+
+$agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)";
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://newsapi.org/v2/everything?q=mocktail&apiKey=f72e3318d86b4a52b2eea095123bc312',
+  CURLOPT_USERAGENT = $agent,
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
