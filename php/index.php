1540c76340b1d6e3e5b2c82aea25bdd7336648c9
AlixNom <alixzulueta@gmail.com>
2024-06-09 16:41:09 +0100

    .

--- php/index.php
+++ php/index.php
@@ -46,7 +46,7 @@ session_start();
             <h2>Enjoy a cocktail with love ones!</h2>
             <div class="line"></div>
             <h1>LIVE LAUGH LOVE!</h1>
-            <a href="" class="ctn">Newsletter</a>
+            <a id = "newsletter"href="" class="ctn">Newsletter</a>
         </div>
     </header>
     <section class="cocktail-section">
