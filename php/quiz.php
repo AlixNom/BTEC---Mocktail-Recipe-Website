cace68bb8afec5f5a612b136863ef3dbcf77bf30
AlixNom <alixzulueta@gmail.com>
2024-06-09 16:48:41 +0100

    .

---
+++ php/quiz.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+//require_once('includes/register_insert.php')
+//include('includes/register_insert.php');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+<div class='nav'>
+        <div class='logo'>
+            Logo
+        </div>
+        <div class='nav-links'>
+            <a class="links" href="index.php">Home</a>
+            <a class="links" href="login.php">Login</a>
+            <a class="links" href="register.php">Not a Member?</a>
+        </div>
+    
+</div>
+
+<?php
+if(isset($_SESSION['status-warning'])) {
+?>
+    <div class="alert-error">
+        <strong>Invalid!</strong> <?php echo $_SESSION['status-warning'];?>
+    </div>
+<?php unset($_SESSION['status-warning']); } ?>
+
+    <div class='login'>
+            <div class='form form-box'>
+                <h3>Mocktail Knowledge</h3>
+                <form action=''>
+                    <h2 id = "question">Question?</h2>
+                    <div class='answer-buttons'>
+                        <button class ="btn">Answer 1</button>
+                        <button class ="btn">Answer 2</button>
+                        <button class ="btn">Answer 3</button>
+                        <button class ="btn">Answer 4</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    <!--<a href='subscribe.php'>Subscribe</a>-->";
+    <section class = "footer">
+        <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
+        <p>Copyright © 2024 Alexis Zulueta</p>
+    </section>
+</body>
+</body>
+</html>
+<script type="text/javascript" src="onload.js"></script>
