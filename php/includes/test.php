c08204a67a6eaecfdb3764f213b923893525314e
AlixNom <alixzulueta@gmail.com>
2024-06-10 15:03:27 +0100

    .

--- php/includes/test.php
+++ php/includes/test.php
@@ -4,26 +4,26 @@ include 'ConnDB.php';
 
 if (isset($_SESSION['user'])){
         echo`tes}`;
-        $id = stripslashes($_SESSION['user']);
-        $id = mysqli_real_escape_string($conn, $id);
+        // $id = stripslashes($_SESSION['user']);
+        // $id = mysqli_real_escape_string($conn, $id);
 
-        $userVal = "SELECT subscribe from mocktail_users WHERE id = '$id' AND subscribe = 'No'";
-        $stmt = mysqli_query($conn, $userVal);
-        if (!$stmt) {
-            die('Query Error: ' . mysqli_error($conn));
-        }
+        // $userVal = "SELECT subscribe from mocktail_users WHERE id = '$id' AND subscribe = 'No'";
+        // $stmt = mysqli_query($conn, $userVal);
+        // if (!$stmt) {
+        //     die('Query Error: ' . mysqli_error($conn));
+        // }
         
-        $count = mysqli_num_rows($stmt);
-        if( $count === 0){
-            $sql = "SELECT * from mocktail_users where id = '$id'";
-            $stmt = mysqli_query($conn, $sql);
-            $count1 = mysqli_num_rows($stmt);
-            echo`row count ${$count}`;
-            if( $count1 === 0){
-                $_SESSION['status-warning'] = "You must register first before you can subscribe subscribe";
-                header("Location: ../register.php");
-            }else{
-                header("Location: ../subscribe.php");}}
+        // $count = mysqli_num_rows($stmt);
+        // if( $count === 0){
+        //     $sql = "SELECT * from mocktail_users where id = '$id'";
+        //     $stmt = mysqli_query($conn, $sql);
+        //     $count1 = mysqli_num_rows($stmt);
+        //     echo`row count ${$count}`;
+        //     if( $count1 === 0){
+        //         $_SESSION['status-warning'] = "You must register first before you can subscribe subscribe";
+        //         header("Location: ../register.php");
+        //     }else{
+        //         header("Location: ../subscribe.php");}}
          }else{
             $_SESSION['status-warning'] = "You must be logged in to view your recipe";
             header("Location: ../login.php");
