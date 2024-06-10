3ebf92752a63165d7098517e6ff823821049fa2f
AlixNom <alixzulueta@gmail.com>
2024-06-10 18:32:57 +0100

    .

--- php/register.php
+++ php/register.php
@@ -32,9 +32,7 @@
     ?>
     <div class="alert-error">
         <strong>Invalid!</strong> <?php echo $_SESSION['status-warning'];?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
+
     </div>
     <?php unset($_SESSION['status-warning']); } ?>
     <div class="login">
