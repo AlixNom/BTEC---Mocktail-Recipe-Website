5835afa680b82ca447f3cf5f5b99584d15b403b5
AlixNom <alixzulueta@gmail.com>
2024-06-10 12:17:08 +0100

    .

--- php/includes/subscribe-navigation.php
+++ php/includes/subscribe-navigation.php
@@ -6,11 +6,8 @@ $id = mysqli_real_escape_string($conn, $id);
 $defaultVal = 'No';
 
 $userVal = "SELECT subscribe from mocktail_users WHERE id = '$id' AND subscribe = '${defaultVal}'";
-
-
     $stmt = mysqli_query($conn, $userVal);
     $count = mysqli_num_rows($stmt);
-
     if( $count === 0){
         $sql = "SELECT * from mocktail_users where id = '$id'";
         $stmt = mysqli_query($conn, $sql);
@@ -21,6 +18,7 @@ $userVal = "SELECT subscribe from mocktail_users WHERE id = '$id' AND subscribe 
          }else{
             $seasontmp = $_GET['season'];
             $_SESSION['season'] = $seasontmp;
+            echo"test";
             header("Location: ../subscribe.php");}};
 
 
