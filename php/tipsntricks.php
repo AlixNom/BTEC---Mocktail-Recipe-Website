e4c4c945d428743f32427c285b8dd546bb4210df
AlixNom <alixzulueta@gmail.com>
2024-06-10 17:12:26 +0100

    iframe

--- php/tipsntricks.php
+++ php/tipsntricks.php
@@ -31,7 +31,9 @@
         <button class="owned" onclick = "location.href = 'index.php'">Back</button>
         <h4>Explore</h4>
     </div>
-   
+    <div class = "videos">
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/6JQDazdbZ4w?si=a6724x1jvCGlsCkC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
     </section>
     <section class = "footer">
         <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
