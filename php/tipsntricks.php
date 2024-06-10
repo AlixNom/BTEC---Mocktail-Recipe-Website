d721c6a44d0085107930ff9a3f2217bdee107fea
AlixNom <alixzulueta@gmail.com>
2024-06-10 17:50:31 +0100

    .

--- php/tipsntricks.php
+++ php/tipsntricks.php
@@ -32,13 +32,25 @@
         <h2>Explore</h2>
     </div><br></br>
     <div class = "videos">
+        <h3>Why Texture matters!</h3><br>
+        <p>Timo Torner emphasizes that texture plays a critical role in a memorable mocktail, and that it's critical to "consider the texture of the drink when alcohol is removed." A good mocktail should have a certain mouthfeel that makes the experience more satisfying and complete. In traditional cocktails, the viscosity of certain spirits contributes to the drink's overall texture. When alcohol is removed from the equation, it's crucial to find alternative ingredients that can give your mocktail a similar depth and body.A bit of coconut milk or cream can lend a tropical richness, making each sip feel like an indulgence. Similarly, a small amount of melted, unsalted butter can add a velvety dimension to warmer drinks, making them perfect for cozy evenings. But be cautious with quantities; you're aiming for a subtle touch, not an overwhelming presence.
+
+Read More: https://www.mashed.com/1381458/tips-experts-how-make-standout-mocktails/ 
+
+Read More: https://www.mashed.com/1381458/tips-experts-how-make-standout-mocktails/</p>
+    </div><br></br>
+    <div class = "videos">
         <h3>Healthy Mocktails Tips!</h3><br>
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/6JQDazdbZ4w?si=a6724x1jvCGlsCkC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div><br></br>
     <div class = "videos">
         <h3>4 Mocktails to try at Home!</h3><br>
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/5wgvpo-xBnA?si=n7wCQAlvb0jCx7Xz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
+    </div><br></br>
+    <div class = "videos">
+        <h3>Sunrise Mocktail Summer Drink!</h3><br>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/wocu1HTj0Rc?si=oI4-FchIRQq82a1h" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div><br></br>
     </section>
     <section class = "footer">
         <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
