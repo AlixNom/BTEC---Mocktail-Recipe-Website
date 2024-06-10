c776c18607c83fbfa0da653bdfd2fdcf86fee0a1
AlixNom <alixzulueta@gmail.com>
2024-06-10 16:51:02 +0100

    .

---
+++ php/tipsntricks.php
@@ -0,0 +1,43 @@
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-browse.css">
+    <title>Tips and Tricks</title>
+</head>
+
+<body>
+
+    <div class="nav">
+        <div class="logo">
+            Logo
+        </div>
+        <div class="nav-links">
+            <a class="links" href="index.php">Home</a>
+            <a class="links" href="login.php">Login</a>
+            <a class="links" href="register.php">Not a Member?</a>
+        </div>
+    </div>
+    <header>    
+        <div class="header-content">
+            <h2>The Daily News headlines about mocktails!</h2>
+            <div class="line"></div>
+        </div>
+    </header>
+    <section class="cocktail-selection">
+    <div class = "header-container">
+        <button class="owned" onclick = "location.href = 'index.php'">Back</button>
+        <h4>Newsletter</h4>
+    </div>
+   
+    </section>
+    <section class = "footer">
+        <p>East Riding College, Beverley, UK | Phone: +44 74751 15553 | Email: alixzulueta@gmail.com</p>
+        <p>Copyright © 2024 Alexis Zulueta</p>
+    </section>
+<script scr="script.js"></script>
+</body>
+
+</html>
